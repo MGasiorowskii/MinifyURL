1120d46bb1812f1a05aebea8563945f3fd7260bd
Mateusz Gasiorowski <mateusz_gasiorowski@goodyear.com>
2025-02-05 19:55:46 +0000

    feat: add signal to create token

---
+++ app/links/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from links.generator import generate_token
+from links.models import ShortURL
+
+
+@receiver(post_save, sender=ShortURL)
+def create_token(sender, instance, **kwargs):
+    if not instance.token:
+        while True:
+            token = generate_token(instance.id)
+            if not ShortURL.objects.filter(alias=token).exists():
+                break
+
+        instance.token = token
+        instance.save(update_fields=['token'])
