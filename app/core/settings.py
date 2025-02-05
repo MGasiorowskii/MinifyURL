933d9ff7f774640b15f18e93c46323ad0369a7db
Mateusz Gasiorowski <mateusz_gasiorowski@goodyear.com>
2025-02-05 19:57:29 +0000

    refactor: generator to get data from django.settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# APP SETTINGS
+DOMAIN = 'mini.fy'
+TOKEN_LENGTH = 6
--- app/links/generator.py
+++ app/links/generator.py
@@ -1,23 +1,12 @@
 import random
 import string
 
-from links.models import ShortURL
-
-TOKEN_LENGTH = 6
-DOMAIN = 'mini.fy'
+from django.conf import settings
 
 
-def generate_short_link(original_url: str) -> str:
-    while True:
-        token = _generate_token()
-        if not ShortURL.objects.filter(alias=token).exists():
-            break
-
-    ShortURL.objects.create(original=original_url, alias=token)
-    return f'https://{DOMAIN}/{token}'
-
-
-def _generate_token(length: int = TOKEN_LENGTH) -> str:
-    if length < 1:
+def generate_token(idx: int) -> str:
+    if settings.TOKEN_LENGTH < 1:
         raise ValueError('The length of the random string must be greater than 0.')
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return ''.join(
+        random.choices(string.ascii_letters + string.digits, k=settings.TOKEN_LENGTH)
+    )
