72713183874a1493962b3f0eaecd4312cb43e9f9
Mateusz Gasiorowski <mateusz_gasiorowski@goodyear.com>
2025-02-05 20:00:24 +0000

    feat: update ShortURL model, add url property

--- app/links/models.py
+++ app/links/models.py
@@ -1,11 +1,18 @@
+from django.conf import settings
 from django.db import models
+
+MAX_TOKEN_LENGTH = 10
 
 
 class ShortURL(models.Model):
     original = models.URLField(unique=True)
-    token = models.CharField(max_length=10, unique=True, db_index=True)
+    token = models.CharField(max_length=MAX_TOKEN_LENGTH, unique=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     click_count = models.BigIntegerField(default=0)
 
     def __str__(self):
         return self.token
+
+    @property
+    def url(self):
+        return f"https://{settings.DOMAIN}/{self.token}"
