1d6e279f8a92291dc9768bf3b2d95e571db56efd
Mateusz Gasiorowski <mateusz_gasiorowski@goodyear.com>
2025-02-06 20:08:03 +0000

    ci: add docker with postgres db

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,22 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21e798ojw55!y8uwv8!f=tgijgctb5&!9*joub0v#19eh!b)jn'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list(getenv('DJANGO_ALLOWED_HOSTS'))
 
 
 # Application definition
@@ -80,8 +85,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': getenv('POSTGRES_NAME'),
+        'USER': getenv('POSTGRES_USER'),
+        'PASSWORD': getenv('POSTGRES_PASSWORD'),
+        # 'HOST': getenv('POSTGRES_DB', 'localhost'),
+        'HOST': 'localhost',
+        'PORT': getenv('POSTGRES_PORT'),
     }
 }
 
